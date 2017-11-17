76c53bb53a3ec365c908e8d7a9a3bea6057d0349
Stian Sandve <stian@sandve.org>
2017-11-17 16:47:48 +0100

    Changed color of loading button

--- src/GarageOpener.Client/src/app/app.component.ts
+++ src/GarageOpener.Client/src/app/app.component.ts
@@ -10,35 +10,35 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 export class AppComponent {
   opening: boolean;
-  openingTime: number = 6000;
-  buttonText: string = "Open";
-  buttonClass: string = "default";
+  openingTime = 6000;
+  buttonText = 'Open';
+  buttonClass = 'default';
 
   constructor(private garageService: GarageService) {  }
 
   open(): void {
     this.opening = true;
-    this.buttonText = "Opening...";
+    this.buttonText = 'Opening...';
     this.garageService.openDoor().subscribe(
       data => {
         setTimeout(() => {
-          this.buttonClass = "success";
-          this.buttonText = "Drive!"
+          this.buttonClass = 'success';
+          this.buttonText = 'Drive!';
           this.opening = false;
           setTimeout(() => {
-            this.buttonText = "Open";
-            this.buttonClass = "default";
+            this.buttonText = 'Open';
+            this.buttonClass = 'default';
           }, 3000);
         }, this.openingTime);
       },
       (err: HttpErrorResponse) => {
         console.error(`API returned code ${err.status}, body was: ${err.error}`);
-        this.buttonText = "Error";
+        this.buttonText = 'Error';
         this.opening = false;
-        this.buttonClass = "error";
+        this.buttonClass = 'error';
         setTimeout(() => {
-           this.buttonClass = "default";
-           this.buttonText = "Open";
+           this.buttonClass = 'default';
+           this.buttonText = 'Open';
         }, this.openingTime);
       }
     );
